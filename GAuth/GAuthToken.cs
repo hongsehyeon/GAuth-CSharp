4a608c07d6b7fa2a06841f2ed56bd5745d476f62
hsh0517 <pelino0517@gmail.com>
2023-05-25 11:18:44 +0900

    :hammer: :: Add GAuthToken.cs

---
+++ GAuth/GAuthToken.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace GAuth_CSharp
+{
+    public class GAuthToken
+    {
+        public string AccessToken { get; private set; }
+        public string RefreshToken { get; private set; }
+
+        public GAuthToken(Dictionary<string, string> dic)
+        {
+            AccessToken = dic["accessToken"];
+            RefreshToken = dic["refreshToken"];
+        }
+    }
+}
