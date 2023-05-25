ab801f38c635175ffdca01018baf9f5b64326ab6
hsh0517 <pelino0517@gmail.com>
2023-05-25 11:19:11 +0900

    :hammer: :: Add GAuthException.cs

---
+++ GAuth/Core/GAuthException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GAuth_CSharp.Core
+{
+    public class GAuthException : Exception
+    {
+        public int StatusCode { get; private set; }
+
+        public GAuthException(int statusCode)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
