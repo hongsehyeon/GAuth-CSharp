2dfba80eb7d3885cc05a12b4c22db79db51af069
hsh0517 <pelino0517@gmail.com>
2023-05-25 11:18:21 +0900

    :hammer: :: Add GAuth.cs

---
+++ GAuth/GAuth.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace GAuth_CSharp
+{
+    public interface GAuth
+    {
+        Task<GAuthToken> GenerateToken(string email, string password, string clientId, string clientSecret, string redirectUri);
+
+        Task<GAuthToken> GenerateToken(string code, string clientId, string clientSecret, string redirectUri);
+
+        Task<GAuthCode> GenerateCode(string email, string password);
+
+        Task<GAuthToken> Refresh(string refreshToken);
+
+        Task<GAuthUserInfo> GetUserInfo(string accessToken);
+    }
+}
