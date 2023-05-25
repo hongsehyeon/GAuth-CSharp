0a7ceecd48f257380ce8454664527b001543fd1f
hsh0517 <pelino0517@gmail.com>
2023-05-25 11:18:34 +0900

    :hammer: :: Add GAuthCode.cs

---
+++ GAuth/GAuthCode.cs
@@ -0,0 +1,12 @@
+﻿namespace GAuth_CSharp
+{
+    public class GAuthCode
+    {
+        public string Code { get; private set; }
+
+        public GAuthCode(string code)
+        {
+            Code = code;
+        }
+    }
+}
