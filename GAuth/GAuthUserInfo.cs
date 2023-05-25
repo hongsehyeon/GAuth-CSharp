d52ec00ef9eff548bdbe45994594f86c018635a7
hsh0517 <pelino0517@gmail.com>
2023-05-25 11:18:56 +0900

    :hammer: :: Add GAuthUserInfo.cs

---
+++ GAuth/GAuthUserInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace GAuth_CSharp
+{
+    public class GAuthUserInfo
+    {
+        public string Email { get; private set; }
+        public string Name { get; private set; }
+        public int Grade { get; private set; }
+        public int ClassNum { get; private set; }
+        public int Num { get; private set; }
+        public string Gender { get; private set; }
+        public string ProfileUrl { get; private set; }
+        public string Role { get; private set; }
+
+        public GAuthUserInfo(Dictionary<string, object> dic)
+        {
+            Email = (string)dic["email"];
+            Name = (string)dic["name"];
+            Grade = (int)(long)dic["grade"];
+            ClassNum = (int)(long)dic["classNum"];
+            Num = (int)(long)dic["num"];
+            Gender = (string)dic["gender"];
+            ProfileUrl = (string)dic["profileUrl"];
+            Role = (string)dic["role"];
+        }
+    }
+}
